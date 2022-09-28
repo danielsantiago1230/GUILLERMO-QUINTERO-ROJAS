ecb09bc5e4044b6e1556f562c9a38b0ac5a0ef44
santiagoquintero95 <danielsantiago1230@gmail.com>
2022-09-28 08:03:14 -0500

    changes about img works

--- pages/Work/_id.vue
+++ pages/Work/_id.vue
@@ -2,14 +2,26 @@
   <div class="bg-bulding-art bg-cover bg-center h-screen w-full">
     <TopNav :english="english" @handle-language="handleLanguage" />
     <main class="h-full w-full bg-gray-bg bg-opacity-70 flex justify-center items-end">
-      <div class="h-5/6 w-5/6 bg-white flex flex-col items-center">
+      <div class="h-5/6 w-5/6 flex flex-col items-center" :class="id[index] === undefined ? 'bg-error-page bg-cover' : 'bg-white'">
         <img src="../../public/warning.png" alt="warning" class="w-24 mt-6" :class="id[index] === undefined ? 'flex' : 'hidden'">
-        <h1 class="font-grotesk font-extrabold text-base lg:text-3xl xl:text-4xl text-brown-dark">
+        <h1 class="font-grotesk font-extrabold text-base lg:text-3xl xl:text-4xl text-brown-dark" :class="id[index] === undefined ? '' : 'mt-4'">
           {{ id[index] !== undefined ? `${id[index].title}` : english ? 'THIS PAGE COULD NOT BE FOUND' : 'PAGINA NO ENCONTRADA' }}
         </h1>
         <button class="font-montserrat mt-4 py-1 md:text-base xl:text-lg text-xs rounded-lg px-2 h-auto w-auto drop-shadow-md text-brown-button bg-gray-bg" :class="id[index] !== undefined ? 'hidden': 'flex'" @click="() => $router.push('/work')">
           {{ english ? 'GO BACK' : 'REGRESAR' }}
         </button>
+        <div class="w-full h-5/6 flex-row justify-center" :class="id[index] === undefined ? 'hidden' : 'flex'">
+          <div class="w-1/2 h-full bg-slate-300">
+            <div class="w-full h-1/2 bg-blue-200 flex justify-center">
+              <img :src="id[index] !== undefined ? '../../public/work/'+ id[index].images[0] : ''" alt="" class="h-full">
+            </div>
+            <div class="w-full h-1/2 px-4 bg-blue-200 flex justify-start items-center">
+              <img :src="id[index] !== undefined ? `../../public/work/${id[index].images[0]}` : ''" alt="" class="w-1/2 mr-2">
+              <img :src="id[index] !== undefined ? `../../public/work/${id[index].images[0]}` : ''" alt="" class="w-1/2">
+            </div>
+          </div>
+          <div class="w-1/2 h-full bg-blue-200" />
+        </div>
       </div>
     </main>
   </div>
@@ -26,17 +38,20 @@ export default {
         galerias: {
           title: 'GALERIAS',
           subTitle: 'Material: Mineral Lime, Soil, Manure',
-          content: 'A concern arising from the curious and detailed observation of traces and footprints. The face of time revealing its secrets through thick and fragmented layers of limestone paint. Color zones that reflect the diversity of the collective spirit of a region with its own characteristics. There, a segment made up of a series of volumetric modules is present in which its construction and design reflect the abstraction of an architecture based on the interception or intersection of the spaces of the house and the street. In a certain way we could be witnessing the visual, imaginary and sensitive exploration of a set of elements that place us in a particular sector of life, existing within the city, which can be that hidden city, or suddenly unnoticed.'
+          content: 'A concern arising from the curious and detailed observation of traces and footprints. The face of time revealing its secrets through thick and fragmented layers of limestone paint. Color zones that reflect the diversity of the collective spirit of a region with its own characteristics. There, a segment made up of a series of volumetric modules is present in which its construction and design reflect the abstraction of an architecture based on the interception or intersection of the spaces of the house and the street. In a certain way we could be witnessing the visual, imaginary and sensitive exploration of a set of elements that place us in a particular sector of life, existing within the city, which can be that hidden city, or suddenly unnoticed.',
+          images: ['galerias-1.jpg', 'galerias-2.jpg', 'galerias-3.jpg']
         },
         'rio-de-oro': {
           title: 'RÍO DE ORO',
           subTitle: 'Material: Earth,Glass ,Audios',
-          content: '"RIO DE ORO" It is a work that can be considered in its appreciation as the result of a procedure aimed at building an action based on the use of resources arranged by the appropriation systems with states, spaces and elements of nature belonging to a geography and particular ways of life. "Inside the room, outside in the park, this complex work is associated with an awareness of the land and the territory, with a rediscovery of its sounds, its signs, its vital signs. At the same time it is related to an urban and creating a tension between the work of nature and the work of man".'
+          content: '"RIO DE ORO" It is a work that can be considered in its appreciation as the result of a procedure aimed at building an action based on the use of resources arranged by the appropriation systems with states, spaces and elements of nature belonging to a geography and particular ways of life. "Inside the room, outside in the park, this complex work is associated with an awareness of the land and the territory, with a rediscovery of its sounds, its signs, its vital signs. At the same time it is related to an urban and creating a tension between the work of nature and the work of man".',
+          images: ['galerias-1.jpg', 'galerias-2.jpg', 'galerias-3.jpg']
         },
         madreselva: {
           title: 'MADRESELVA',
           subTitle: 'Material: Wood, Charcoal, Concrete',
-          content: 'It is a work that, due to its area, form, technique, materials and especially for its exploratory awareness, transcends the limits of traditional museum spaces, managing to advance towards a strategic location in public space. It contains the essential elements of denunciation and a critical spirit, proposing in its passing the renewal and multiplication of its own spectator. MADRESELVA , from its pre-intentional metaphorical communication, alludes to the landscape and an atmosphere of chaos and self-destruction, originating in the exorbitant and uncontrollable urban centers. Characterized by its three-dimensionality and notable sculptural presence, it reiterates the fundamental commitment to respect nature, presenting an interrogative image of the deplorable behavior of the human race.'
+          content: 'It is a work that, due to its area, form, technique, materials and especially for its exploratory awareness, transcends the limits of traditional museum spaces, managing to advance towards a strategic location in public space. It contains the essential elements of denunciation and a critical spirit, proposing in its passing the renewal and multiplication of its own spectator. MADRESELVA , from its pre-intentional metaphorical communication, alludes to the landscape and an atmosphere of chaos and self-destruction, originating in the exorbitant and uncontrollable urban centers. Characterized by its three-dimensionality and notable sculptural presence, it reiterates the fundamental commitment to respect nature, presenting an interrogative image of the deplorable behavior of the human race.',
+          images: ['galerias-1.jpg', 'galerias-2.jpg', 'galerias-3.jpg']
         }
       },
       index: '',
